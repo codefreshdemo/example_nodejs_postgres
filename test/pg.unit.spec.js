0b8c96bbfb82150f5eeec946348a7f6f58d1b684
nikolai <n.ivanov.p@gmail.com>
2017-06-14 15:35:53 +0300

    volumes postgres data - 11

--- test/pg.unit.spec.js
+++ test/pg.unit.spec.js
@@ -18,18 +18,8 @@ describe('postgres test', () => {
         client.end();
     });
 
-    it('test connection', (done) => {
-        client.connect(function(err) {
-            if (err) {
-                console.error('error connecting: ' + err.stack);
-                throw new Error(err);
-            }
-            console.log('connected as id ' + client.threadId);
-            done();
-        });
-    });
-
     it('create a table', () => {
+        client.connect();
         const query = client.query(
             'CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
         query.on('end', () => { client.end(); });
@@ -37,13 +27,6 @@ describe('postgres test', () => {
 
 
     it('insert data', ()=>{
-        // SQL Query > Update Data
-        // client.query('INSERT INTO items(text, complete) values($1, $2)', ['test', true]);
-        // // SQL Query > Select Data
-        // const query = client.query("SELECT * FROM items");
-        // pg.exec(query, function (table) {
-        //     console.log('Result table:', table);
-        // });
         const results = [];
         pg.connect(connectionString, (err, client, done) => {
             // Handle connection errors
@@ -54,16 +37,12 @@ describe('postgres test', () => {
             }
             // SQL Query > Insert Data
             client.query('INSERT INTO items(text, complete) values($1, $2)', ['test', true]);
-            // SQL Query > Select Data
             const query = client.query('SELECT * FROM items');
-            // Stream results back one row at a time
-            query.on('row', (row) => {
-                results.push(row);
-                console.log("res:" + row);
+            query.on('row', (row, result) => {
+                result.addRow(row);
             });
-            // After all data is returned, close connection and return results
-            query.on('end', () => {
-                console.log("results:" + results[0]);
+            query.on('end', (result) => {
+                console.log("results:" + JSON.stringify(result.rows, null, " "));
                 client.end();
             });
         });
