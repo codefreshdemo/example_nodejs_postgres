6c00dc8c4138b72dc0c2dd4886e8a0f951a1065e
nikolai <n.ivanov.p@gmail.com>
2017-06-14 14:38:25 +0300

    volumes postgres data - 2

--- test/pg.unit.spec.js
+++ test/pg.unit.spec.js
@@ -37,6 +37,9 @@ describe('postgres test', () => {
 
     it('select all items', () => {
         const query = client.query("SELECT table_name FROM information_schema.tables WHERE table_schema='public' AND table_type='BASE TABLE';");
+        query.on('row', (row) => {
+            console.log(row);
+        });
         query.on('end', () => { client.end(); });
     });
 });
